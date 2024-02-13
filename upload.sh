a7af8772cf793fc9511c9e6a001885d4966b25e4
Luiz Fellipe Carneiro <luiz@fellipec.com>
2024-02-13 20:39:31 -0300

    Converts URL automatically

--- upload.sh
+++ upload.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 
-echo "Changing the URL to Clementine"
+echo -e "\nChanging the URL to Clementine"
 sed -i -- 's/raw\.githubusercontent\.com\/fellipec\/customshell\/main/clementine\.fellipec\.com\/cs/g' webinstall.sh
 
 echo "Uploading new custom environment installer..."
 rsync -av --del --exclude .git ./ fellipec@clementine.vpn:/var/www/html/cs/
 
-echo "Reverting back to the Github URL
+echo -e "\nReverting back to the Github URL"
 sed -i -- 's/clementine\.fellipec\.com\/cs/raw\.githubusercontent\.com\/fellipec\/customshell\/main/g' webinstall.sh
