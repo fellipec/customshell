66d1e6f6012dcffaf4e24cf6d1259be5229bd83c
Luiz Fellipe Carneiro <luiz@fellipec.com>
2024-02-13 21:08:46 -0300

    Better comments

--- upload.sh
+++ upload.sh
@@ -1,10 +1,13 @@
 #!/bin/bash
 
+#Changes the URLs to work with Clementine server, uploads the files through the VPN
+#and revert the URLs back to the Github defaults.
+
 echo -e "\nChanging the URL to Clementine"
-sed -i -- 's/raw\.githubusercontent\.com\/fellipec\/customshell\/main/clementine\.fellipec\.com\/cs/g' webinstall.sh
+sed -i 's/raw\.githubusercontent\.com\/fellipec\/customshell\/main/clementine\.fellipec\.com\/cs/g' webinstall.sh
 
 echo "Uploading new custom environment installer..."
 rsync -av --del --exclude .git ./ fellipec@clementine.vpn:/var/www/html/cs/
 
 echo -e "\nReverting back to the Github URL"
-sed -i -- 's/clementine\.fellipec\.com\/cs/raw\.githubusercontent\.com\/fellipec\/customshell\/main/g' webinstall.sh
+sed -i 's/clementine\.fellipec\.com\/cs/raw\.githubusercontent\.com\/fellipec\/customshell\/main/g' webinstall.sh
--- webinstall.sh
+++ webinstall.sh
@@ -1,6 +1,30 @@
 #!/bin/bash
 
+# This script installs a custom enviroment based on the ZSH Shell. 
+# In addition it installs the following packages:
+
+# - [ZSH](https://www.zsh.org/)
+#   - [Oh My Zsh](https://ohmyz.sh/)
+#   - [Powerlevel10k](https://github.com/romkatv/powerlevel10k)
+# - [Command-not-found](https://tracker.debian.org/pkg/command-not-found)
+# - [Byobu](https://www.byobu.org/)
+# - [LSDeluxe](https://github.com/lsd-rs/lsd)
+# - [Bat](https://github.com/sharkdp/bat)
+# - [Duf](https://github.com/muesli/duf)
+# - [Htop](https://htop.dev/)
+# - [Btop](https://github.com/aristocratos/btop)
+# - [Wget](https://www.gnu.org/software/wget/)
+# - [cURL](https://curl.se/)
+# - [Git](https://git-scm.com/)
+# - [tldr-pages](https://github.com/tldr-pages/tldr)
+# - [Sakura](https://github.com/dabisu/sakura)
+# - [GParted](https://gparted.org/)
 #
+# More information on https://github.com/fellipec/customshell
+#
+
+
+#Make sure the system is updated
 echo -e "================================="
 echo -e "Configuring the custom enviroment"
 echo -e "================================="
@@ -9,7 +33,7 @@ sudo apt update
 sudo apt upgrade
 sudo apt autoremove
 
-
+#Install the packages that work on CLI
 echo -e "\n\nInstalling packages..."
 INSTALL_PKGS="zsh zsh-common zsh-doc zsh-autosuggestions command-not-found byobu lsd bat duf htop btop wget curl git tldr"
 for i in $INSTALL_PKGS; do
@@ -21,9 +45,12 @@ done
 #yt-dlp from the repos are not always update
 echo -e "\nInstalling/updating yt-dlp..."
 sudo curl -L https://github.com/yt-dlp/yt-dlp/releases/latest/download/yt-dlp --output /usr/local/bin/yt-dlp
+
 #before tldr can be used, it needs to be updated
-echo -e "\nUpdating tldr..."
-tldr --update
+if [[ $(dpkg-query -W -f='${Status}' tldr 2>/dev/null | grep -c "ok installed") -eq 1 ]]; then 
+    echo -e "\nUpdating tldr..."
+    tldr --update
+fi
 
 #Install the GUI apps only if in a x11 or wayland session
 if [[ $XDG_SESSION_TYPE == 'x11' || $XDG_SESSION_TYPE == 'wayland' ]]; then
@@ -35,6 +62,8 @@ if [[ $XDG_SESSION_TYPE == 'x11' || $XDG_SESSION_TYPE == 'wayland' ]]; then
             sudo apt-get install -y $i
         fi
     done
+
+    #Download sakura configuration and make it the default terminal if it was installed
     if [[ $(dpkg-query -W -f='${Status}' sakura 2>/dev/null | grep -c "ok installed") -eq 1 ]]; then 
         echo -e "\n\nConfiguring Sakura terminal"
         if ! [[ -e ~/.config/sakura ]]; then
@@ -55,10 +84,12 @@ if [[ $XDG_SESSION_TYPE == 'x11' || $XDG_SESSION_TYPE == 'wayland' ]]; then
     fi
 fi
 
+#Backups the current bashrc and zshrc
 echo -e "\n\nBackuping current config..."
 cp ~/.bashrc ~/.bashrc.bkp
 cp ~/.zshrc ~/.zshrc.bkp
 
+#Checks for oh-my-zsh and install if needed
 if ! [[ -e ~/.oh-my-zsh ]]; then
     echo -e "\n\nInstalling Oh My Zsh! \n Type exit after install is completed"
     sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
@@ -66,6 +97,7 @@ else
     echo -e "\nOh My Zsh already installed, skipping..."
 fi
 
+#Checks for Powerlevel10k and install if needed
 if ! [[ -e ~/.oh-my-zsh/custom/themes/powerlevel10k ]]; then
     echo -e "\n\nInstalling Powerlevel10k..."
     git clone --depth=1 https://github.com/romkatv/powerlevel10k.git ${ZSH_CUSTOM:-$HOME/.oh-my-zsh/custom}/themes/powerlevel10k
@@ -73,24 +105,24 @@ else
     echo -e "\n\nPowerlevel10k already installed, skipping..."
 fi
 
-
+#Download the custom configuration for ZSH and Powerlevel10k
 echo -e "\n\nDownloading new configuration..."
 curl -L https://raw.githubusercontent.com/fellipec/customshell/main/zshrc --output ~/.zshrc
 curl -L https://raw.githubusercontent.com/fellipec/customshell/main/bashrc --output ~/.bashrc
 
 
-
+#Change the default shell to ZSH (If is not already)
 if [[ $SHELL != '/usr/bin/zsh' ]]; then
     echo -e "\n\nChanging the default shell to zsh..."
     chsh -s $(which zsh)
 fi
 
+#User selection of the Powerlevel10k theme. 
 echo -e "\n\nChoose the p10k config:\n"
 echo -e "    1) Black (default)"
 echo -e "    2) Color"
 echo -e "    3) Laptop"
 echo -e "    4) Full"
-
 
 read -p "Pick an option: " PTKFLAVOR
 
@@ -108,6 +140,7 @@ else
     curl -L https://raw.githubusercontent.com/fellipec/customshell/main/p10k.zsh.black --output ~/.p10k.zsh
 fi
 
+#User selection to install autofs and configure to use /net folder. Also adds scarlett.lan to hosts for good measure
 echo -e "Install autofs?"
 
 read -p "y/[n]" INST_AUTOFS
