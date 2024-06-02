e41804c80d06afca3a3fb30fc2ae096ddd8607e3
Luiz Fellipe Carneiro <fellipec@outlook.com>
2024-06-02 12:15:58 -0300

    Changed SFTP endpoint

--- upload.sh
+++ upload.sh
@@ -7,7 +7,7 @@ echo -e "\nChanging the URL to Clementine"
 sed -i 's/raw\.githubusercontent\.com\/fellipec\/customshell\/main/clementine\.fellipec\.com\/cs/g' webinstall.sh
 
 echo "Uploading new custom environment installer..."
-rsync -av --del --exclude .git ./ fellipec@clementine.vpn:/var/www/html/cs/
+rsync -av --del --exclude .git ./ fellipec@ssh.fellipec.com:/var/www/html/cs/
 
 echo -e "\nReverting back to the Github URL"
 sed -i 's/clementine\.fellipec\.com\/cs/raw\.githubusercontent\.com\/fellipec\/customshell\/main/g' webinstall.sh
